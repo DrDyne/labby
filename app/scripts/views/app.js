20126d9a86c4f510a14493fc428b9558194b670f
remy <remy_chaumard@silanis.com>
2013-12-25 18:46:44 -0500

    renamed game.js to app.js and created a new game.js view... git was confused, sorry

---
+++ app/scripts/views/app.js
@@ -0,0 +1,50 @@
+define([
+  'backbone',
+  'com',
+  'session',
+  'models/player',
+  'views/game',
+  'views/app-splashScreen',
+  'views/app-menu',
+], function (
+  Backbone,
+  com,
+  session,
+  Player,
+  Game,
+  SplashScreen,
+  Menu
+) {
+  return Backbone.View.extend({
+    events: {
+      'submit #welcome-menu': 'onPlayerCreate',
+    },
+
+    initialize: function (options) {
+      this.splashScreen = new SplashScreen({el: '#body'});
+      this.menu = new Menu({el: "#menu"});
+      this.game = new Game({el: '#game'});
+
+      this.splashScreen.start();
+      com.on('players', this.onPlayers.bind(this));
+      com.emit('game:init', this.id);
+    },
+
+    onPlayerCreate: function (event) {
+      event.preventDefault();
+      var playerName = this.$el.find('#player-name').val();
+      com.emit('player:create', playerName);
+      session.set('player', new Player({id: playerName}));
+      this.menu.show();
+      this.hideWelcomeMenu();
+    },
+
+    hideWelcomeMenu: function () {
+      this.$el.find('#welcome-menu').slideUp();
+    },
+
+    onPlayers: function (options) {
+      this.$el.find('p.player-list').html(options.join(', '));
+    },
+  });
+});
--- app/scripts/views/game.js
+++ app/scripts/views/game.js
@@ -1,51 +1,33 @@
 define([
   'backbone',
   'com',
+  'session',
   'models/game',
-  'views/splashScreen',
   'views/map',
   'views/game-hud',
-  'views/game-menu',
-], function (Backbone, com, Game, SplashScreen, Map, Hud, Menu) {
+], function (Backbone, com, session, Game, Map, Hud, Layouts) {
   return Backbone.View.extend({
-    events: {
-      'submit #welcome-menu': 'onPlayerCreate',
-      'change #select-stage': 'onSelectStage',
-    },
-
     initialize: function (options) {
-      this.splashScreen = new SplashScreen();
-      this.splashScreen.start();
-      com.on('players', this.com.onPlayers.bind(this));
-      com.emit('game:init', this.id);
+      com.on('game:create', this.createGame.bind(this));
+      com.on('game:join', this.joinGame.bind(this));
     },
 
-    createMap: function (stage) {
-      this.map = new Map({layout: stage});
+    createGame: function (options) {
+      this.map = new Map({el: '#map', layout: options.stage});
+
+      this.game = new Game({
+        name: options.name,
+        stage: options.stage,
+      });
+      this.game.addPlayer(session.get('player'));
+
       this.map.render();
+      com.emit('game:created', this.game.toJSON());
     },
 
-    waitingForPlayer: function () {
-      alert('waiting for players...');
+    joinGame: function (options) {
+      console.log('join game', options);
     },
 
-    onPlayerCreate: function (event) {
-      event.preventDefault();
-      var playerName = this.$el.find('#player-name').val();
-      com.emit('player:create', playerName);
-      this.$el.find('.select-stage-container').removeClass('hidden');
-    },
-
-    onSelectStage: function (event) {
-      var selectedStage = this.$el.find('#select-stage').val();
-      this.createMap(selectedStage);
-      this.waitingForPlayer();
-    },
-
-    com: {
-      onPlayers: function (options) {
-        this.$el.find('p.player-list').html(options.join(', '));
-      },
-    },
   });
 });
