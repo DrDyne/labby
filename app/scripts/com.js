b565fd6130f1a899ddc88c047ac6ec3e78120daf
remy <remy_chaumard@silanis.com>
2013-12-30 09:06:05 -0500

    isolated ws com layer in the com module so that views have only access to cloned Backbone.Events and only com module is responsible for broadcasting events and keeping track of the connection status

--- app/scripts/com.js
+++ app/scripts/com.js
@@ -3,6 +3,20 @@ define([
   'http://localhost:9001/socket.io/socket.io.js'
 ], function (Backbone, io) {
   var com = _.clone(Backbone.Events);
-  com.ws = window.io.connect('http://localhost:9001');
+  var ws = window.io.connect('http://localhost:9001');
+
+  com.on('app:init', function (options) { ws.emit('app:init', options) });
+  com.on('player:create', function (options) { ws.emit('player:create', options) });
+  com.on('lobby:games', function (options) { ws.emit('lobby:games', options) });
+  com.on('game:join', function (options) { ws.emit('game:join', options) });
+
+  com.on('game:created', function (options) { ws.emit('game:created', options) });
+
+  ws.on('player:moved', function (options) { com.trigger('player:moved', options) });
+  ws.on('players:connected', function (options) { console.log('connected players:', options); com.trigger('players:connected', options) });
+  ws.on('lobby:games:existing', function (options) {
+    com.trigger('lobby:games:existing', options);
+  });
+
   return com;
 });
