d2adaa875a44623d79426cf1c0298423238d52d9
remy <remy_chaumard@silanis.com>
2014-01-04 11:16:26 -0500

    now positionning squares with css (absolute positionning allows pushing rows and cols) -- added handlers to views/map to animate map add/push:row/push:col/remove

--- app/scripts/views/map.js
+++ app/scripts/views/map.js
@@ -12,17 +12,36 @@ define([
       this.collection.loadLayout(options.layout);
       this.players = new Players({el: options.el, collection: this.collection, model: this.model.getPlayers()});
       this.chrome = new Chrome({el: options.el, collection: this.collection})
+
+      this.render();
+
+      this.listenTo(this.collection, 'add', this.animateAdd, this);
+      this.listenTo(this.collection, 'push:row', this.animatePushRow, this);
+      this.listenTo(this.collection, 'push:col', this.animatePushCol, this);
+      this.listenTo(this.collection, 'remove', this.animateRemove, this);
+    },
+
+    squareWidth: 50,
+
+    mapStyle: function (element) {
+      var options = {};
+      options.width = options.height = (this.collection.width * this.squareWidth) + 'px';
+      return options;
     },
 
     render: function () {
       var self = this, surface = this.$el.find('.map-surface');
+
+      surface.css(this.mapStyle());
 
       surface.html('');
 
       _(this.collection.rows()).each(function (row, index) {
         row.index = index;
         var mapRow = $(tpl.map.row(row));
-        _(row).each(function (square) { mapRow.append(tpl.map.square(square.toJSON())) });
+        _(row).each(function (square) {
+          mapRow.append(tpl.map.square(square.toJSON()));
+        });
         surface.append(mapRow);
       });
 
@@ -36,7 +55,26 @@ define([
     },
 
     renderWalls: function (pos) {
-      console.log('render walls', pos.x, pos.y);
+      //console.log('render walls', pos.x, pos.y);
+    },
+
+
+    // animate push of row/col
+    // re-render map
+    animateAdd: function (options) {
+      console.log('add', options);
+    },
+
+    animatePushRow: function (options) {
+      console.log('push row', options);
+    },
+
+    animatePushCol: function (options ) {
+      console.log('push col', options);
+    },
+
+    animateRemove: function (options ) {
+      console.log('remove', options);
     },
 
   });
